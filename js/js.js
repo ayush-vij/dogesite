8517acda1fb1853d685ffc63d60cb64461c896f4
Ayush Vij <ayushvij@live.in>
2021-05-09 03:32:14 -0700

    Version 1.0 completed

--- js/js.js
+++ js/js.js
@@ -21,8 +21,59 @@ async function getapi(url) {
     var textbeforertprice = "Right now, DOGE is worth: $ ";
     document.getElementById("currentpricedoge").innerHTML = textbeforertprice += text.nodeValue;
     if (response) {}
+
+    //table thing
+    let displayResources = document.querySelector("#table");
+    displayResources.textContent =
+        "Loading data from JSON source...";
+    fetch('../assets/data.json')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(result) {
+            let totalam = 0;
+            let totaldc = 0
+            let totalrate = 0;
+            let avgrate = 0;
+            let pl = 0;
+            let output =
+                "<table><thead><tr><th>Date</th><th>Quantity</th><th>Rate</th><th>Amount</th></thead><tbody>";
+            for (let i in result) {
+                output +=
+                    "<tr><td>" +
+                    result[i].Date +
+                    "</td><td>" +
+                    result[i].Qty +
+                    "</td><td>" +
+                    result[i].Rate +
+                    "</td><td>" +
+                    result[i].SpentAmount +
+                    "</td></tr>";
+                totaldc = totaldc + result[i].Qty;
+                totalam = totalam + result[i].SpentAmount;
+                totalrate = totalrate + result[i].Rate; //to be used for avgrate
+                avgrate = totalrate / (result.length);
+                let currentvalue = 0;
+                currentvalue = (text.nodeValue) * totaldc;
+                pl = currentvalue - totalam;
+
+            }
+            document.getElementById('totaldc').innerHTML = totaldc;
+            document.getElementById('totalamt').innerHTML = totalam.toFixed(3);
+            document.getElementById('avgrate').innerHTML = avgrate.toFixed(4);
+            document.getElementById('plfield').innerHTML = pl.toFixed(4);
+            // console.log("Total" + total);
+            // output += "</tbody></table>";
+            // let tabletwo =
+            //     "<table><thead><tr><th>Total Amount Invested</th></tr></thead><tbody>";
+            // tabletwo += "<tr><td>" + total + "</td></tr>";
+            // tabletwo += "</tbody></table>";
+
+
+            displayResources.innerHTML = output;
+        });
 }
-setInterval(function() { getapi(api_url) }, 500);
+setInterval(function() { getapi(api_url) }, 2000);
 // document.write("<h1>" + getapi(api_url).text + "</h1>");
 // var funcall = getapi(api_url);
 // var timer = setInterval(1000); // Calling that async function
